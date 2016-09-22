60052bdf53111917843552ec0c6a40b106e4f4d9
Benjamin Mood <bmood@ptloma.edu>
2016-09-22 09:45:25 -0700

    adding first commit

---
+++ random project/Assets/ballContoller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballContoller : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 newposition = Vector3.zero; //= new Vector3();
+
+        newposition.z = 0.1f;
+
+        /*newposition.x = transform.position.x;
+        newposition.y = transform.position.y+0.11f;
+        newposition.z = transform.position.z;
+
+        transform.position = newposition;*/
+
+        Rigidbody m = GetComponent<Rigidbody>();
+        m.AddForce(newposition);
+
+
+        //transform.position = 
+
+  
+
+	}
+}
---
+++ random project/Assets/cubescript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubescript : MonoBehaviour {
+
+
+    public Object o;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    float lasttime;
+	// Update is called once per frame
+	void Update () {
+	
+        if(lasttime +0.1f < Time.time)
+        {
+            Vector3 newposition = transform.position;
+
+            newposition.y += 10;
+            newposition.x += Random.Range(-0.1f, 0.1f);
+            newposition.y += Random.Range(-0.1f, 0.1f);
+            newposition.z += Random.Range(-0.1f, 0.1f);
+
+            Object temp = Instantiate(o, newposition, Quaternion.identity);
+
+            Destroy(temp, 5);
+
+            lasttime = Time.time;
+        }
+
+
+	}
+}
